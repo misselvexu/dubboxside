103433c3d5a8b9b2168583c85dd34f2dc03e85e2
misselvexu <x_vivi@yeah.net>
2017-01-10 09:55:44 +0800

    Update DubboServiceSupport.java

--- showcase-provider/src/main/java/xyz/vopen/passport/service/DubboServiceSupport.java
+++ showcase-provider/src/main/java/xyz/vopen/passport/service/DubboServiceSupport.java
@@ -1,7 +1,5 @@
 package xyz.vopen.passport.service;
 
-
-import com.pyw.commons.cache.memcached.MemcachedClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import xyz.vopen.passport.dao.PassportDAO;
